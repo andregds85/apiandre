4f41f9abb85493c7b0eb4390afc7a9e63c1d3792
André <andregds85@gmail.com>
2022-12-09 19:19:42 -0300

    Melhorando a api

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,3 +1,33 @@
+
+<?php
+    use App\Models\Categorias;
+    $tabela = Categorias::all();
+
+?>
+      
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <th>nome</th>
+        </tr>
+	    @foreach ($tabela as $categoria)
+
+        <tr>
+
+            <td>{{$categoria->id}}</td>
+	        <td>{{$categoria->nome}}</td>
+
+        </tr>
+	    @endforeach
+    </table>
+
+
+
+
+
+
+
+
 <?php
     use App\Models\Videos;
     $tabela = videos::all();
